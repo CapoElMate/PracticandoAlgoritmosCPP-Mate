81f9b459da4c55e72b3d81a61faa7cb8715b10d9
CapoElMate <mateotroll460@gmail.com>
2025-07-29 09:10:43 -0300

    cambiocuenta

---
+++ leetcode/LC7-238.ProductofArrayExceptSelf.cpp
@@ -0,0 +1,25 @@
+
+#include<iostream>
+#include <vector>
+using namespace std;
+
+
+class Solution {
+    public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        
+    }
+};
+
+int main()
+{    
+    Solution sol;
+    vector<int> v = {1,2,3,4};
+
+    for(int i : sol.productExceptSelf(v))   {
+        cout << i +", ";
+    }
+
+    
+    return 0;
+}
